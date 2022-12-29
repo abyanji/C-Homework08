26858834ddf221c232e532596bae4ecfc1c7c4bd
abyanji <a.byanjil99@gmail.com>
2022-12-30 01:27:04 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void MinRow(int[,] array)
+{
+    double average = 0;
+    bool firstCycle = true;
+    double min = 0;
+    int minRow = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            average += array[i, j];
+        }
+        average = average / array.GetLength(1);
+        if (firstCycle)
+        {
+            min = average;
+            firstCycle = false;
+        }
+        if (average < min)
+        {
+            min = average;
+            minRow = i;
+        }
+        average = 0;
+    }
+    System.Console.WriteLine($"Minimun row is {minRow + 1}");
+}
+
+int rows = DataEntry("Enter the value of rows: ");
+int columns = DataEntry("Enter the value of columns: ");
+int[,] matrix = CreateRandomArray(rows, columns, 1, 9);
+PrintArray(matrix);
+MinRow(matrix);
